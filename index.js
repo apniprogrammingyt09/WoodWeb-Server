b9843cf6fd1c57b179b8617c9aeddef87f6769c9
KRISH bhagat <93695269+apniprogrammingyt09@users.noreply.github.com>
2025-03-09 10:52:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,16 +2,15 @@ import nodemailer from "nodemailer";
 import express from "express";
 import cors from "cors";
 import env from "dotenv";
+
 const app = express();
 env.config();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
     credentials: true,
   })
 );
-
 
 app.post("/api/questions", (req, res) => {
   const { question, name, phone } = req.body;
@@ -20,7 +19,6 @@ app.post("/api/questions", (req, res) => {
   if (!process.env.SENDER_EMAIL || !process.env.SENDER_PASSWORD) {
     return res.status(500).json({ error: "Missing email credentials in environment variables" });
   }
-
 
   const transporter = nodemailer.createTransport({
     service: "gmail",
@@ -34,14 +32,13 @@ app.post("/api/questions", (req, res) => {
     from: process.env.SENDER_EMAIL,
     to: process.env.RECEIVER_EMAIL,
     subject: "New Question Received",
-    text: `User: ${name}\n Phone: ${phone} \nQuestion: ${question}`,
+    text: `User: ${name}\nPhone: ${phone}\nQuestion: ${question}`,
   };
 
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
       console.log(error);
       console.log("info", info);
-
       return res.status(500).json({ error: error.message });
     }
     res.json({ message: "Question sent via email!" });
